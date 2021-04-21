2c8de6f6d1477b31bf51aa57b79fa3c0032e5961
alwsBgood <dchebotaru@emergemarket.com>
2021-04-21 17:47:17 +0300

    Add homepage

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
       <div className="App">
           <Router>
               <Switch>
-                  <Route path="/start" children={<WelcomeScreen />}/>
+                  <Route path="/" children={<WelcomeScreen />}/>
                   <Route path="/df9ef054-83e0-44da-8f8f-dc571c840378" children={<Step1 />}/>
                   <Route path="/e6394ca4-b004-4907-a37b-20787ff40d0d" children={<Step2 />}/>
                   <Route path="/fe69d4ce-586a-443c-8935-188060d9423e" children={<Step3 />}/>
